faac41dc4d3945c4cfa3b41bfdf92cbf37dd8f28
harshgupta20 <hgreatgupta2001@gmail.com>
2023-03-31 23:14:15 +0530

    Filter section done UI

---
+++ src/components/PizzaCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PizzaCard = () => {
+  return (
+    <div>PizaaCard</div>
+  )
+}
+
+export default PizzaCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import "../styles/Home.css";
+import PizzaCard from '../components/PizzaCard';
 
 const Home = () => {
 
@@ -14,16 +15,18 @@ const Home = () => {
     // console.log("------");
     // console.log(price);
 
+    // https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg
+
     return (
         <>
             <div id="home">
                 <div id="home-filter">
-                    <select onChange={(e) => setPrice(e.target.value)} name="price-filter" id="">
+                    <select id="home-filter-price" onChange={(e) => setPrice(e.target.value)} name="price-filter">
                         <option value="" disabled selected>Select Price</option>
                         <option value="low">Low to High</option>
                         <option value="high">High to Low</option>
                     </select>
-                    <select onChange={(e) => setRating(e.target.value)} name="rating-filter" id="">
+                    <select id="home-filter-rating" onChange={(e) => setRating(e.target.value)} name="rating-filter">
                         <option value="" disabled selected>Select Rating</option>
                         <option value="5">5 Stars</option>
                         <option value="4">4 Stars</option>
@@ -31,13 +34,19 @@ const Home = () => {
                         <option value="2">2 Stars</option>
                         <option value="1">1 Stars</option>
                     </select>
-                    <label className="switch">
-                        <input type="checkbox" onChange={(e) => setVegStatus(e.target.checked)} />
-                        <span className="slider round"></span>
-                    </label>
-
-
+                    <div id="home-filter-veg">
+                        <p>Non-Veg</p>
+                        <label style={{margin:'0 10px'}} className="switch">
+                            <input type="checkbox" onChange={(e) => setVegStatus(e.target.checked)} />
+                            <span className="slider round"></span>
+                        </label>
+                        <p>Veg</p>
+                    </div>
                 </div>
+
+
+                    <PizzaCard />
+
             </div>
         </>
     )
