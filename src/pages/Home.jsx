842bfc39e0cdbac17aa9070c47cff93fda03a7e5
harshgupta20 <hgreatgupta2001@gmail.com>
2023-04-01 22:49:35 +0530

    added filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,30 +9,23 @@ const Home = () => {
 
     const [price, setPrice] = useState();
     const [rating, setRating] = useState();
-    const [vegStatus, setVegStatus] = useState();
+    const [vegStatus, setVegStatus] = useState(false);
     const [cartList, setCartList] = useState([]);
     const [foundInCart, setFoundInCart] = useState(false);
 
     const [data, setData] = useState();
 
-    // console.log(vegStatus);
-    // console.log("------");
-    // console.log(rating);
-    // console.log("------");
-    // console.log(price);
-
-
     useEffect(() => {
-        const cartData  = JSON.parse(localStorage.getItem('cart'));
+        const cartData = JSON.parse(localStorage.getItem('cart'));
         // console.log(typeof(cartData));
         const dataFetch = async () => {
             const fetchData = await axios.get("https://run.mocky.io/v3/ec196a02-aaf4-4c91-8f54-21e72f241b68")
             const temp = fetchData.data;
-            if(cartData !== null){
+            if (cartData !== null) {
                 // console.log("if");
-                for(let i=0; i<cartData.length; i++){
-                    for(let j=0; j<temp.length; j++){
-                        if(cartData[i].id == temp[j].id){
+                for (let i = 0; i < cartData.length; i++) {
+                    for (let j = 0; j < temp.length; j++) {
+                        if (cartData[i].id == temp[j].id) {
                             temp.splice(j, 1);
                         }
                     }
@@ -47,6 +40,25 @@ const Home = () => {
     }, [])
 
 
+
+    const sortAnArray = (array) => {
+        array.sort((a, b) => {
+            if(a.rating === b.rating){
+              // If two elements have same number, then the one who has larger rating.average wins
+              return b.rating - a.rating;
+            } else {
+              // If two elements have different number, then the one who has larger number wins
+              return b.rating - a.rating;
+            }
+          });
+
+          array.filter((element, index) => {
+            return index === 0 || element.rating !== array[index-1].rating;
+          });
+
+          return array;
+    }
+
     // console.log(data);
 
 
@@ -54,12 +66,12 @@ const Home = () => {
         <>
             <div id="home">
                 <div id="home-filter">
-                    <select id="home-filter-price" onChange={(e) => setPrice(e.target.value)} name="price-filter">
+                    <select id="home-filter-price" onChange={(e) => setPrice(e.target.value) } name="price-filter">
                         <option value="" disabled selected>Select Price</option>
                         <option value="low">Low to High</option>
                         <option value="high">High to Low</option>
                     </select>
-                    <select id="home-filter-rating" onChange={(e) => setRating(e.target.value)} name="rating-filter">
+                    <select id="home-filter-rating" onChange={(e) => setRating(e.target.value) } name="rating-filter">
                         <option value="" disabled selected>Select Rating</option>
                         <option value="5">5 Stars</option>
                         <option value="4">4 Stars</option>
@@ -70,7 +82,7 @@ const Home = () => {
                     <div id="home-filter-veg">
                         <p>Non-Veg</p>
                         <label style={{ margin: '0 10px' }} className="switch">
-                            <input type="checkbox" onChange={(e) => setVegStatus(e.target.checked)} />
+                            <input type="checkbox" onChange={(e) =>  setVegStatus(e.target.checked) } />
                             <span className="slider round"></span>
                         </label>
                         <p>Veg</p>
@@ -87,20 +99,21 @@ const Home = () => {
                             })
                         }
                         {
-                            data ? data.map((pizza, key) => {
-                                return (
-                                    <>
-                                        <PizzaCard key={key} id={pizza.id} image={pizza.img_url} name={pizza.name} desc={pizza.description} rating={pizza.rating} price={pizza.price} size={pizza.size} toppings={pizza.toppings} isVeg={pizza.isVeg} cartStatus="false" />
-                                    </>
-                                )
-                            })
+                            data ? data.filter((data) => {
+                                return data.isVeg == vegStatus || data.rating >= rating
+                            }).map((pizza, key) => {
+                                    return (
+                                        <>
+                                            <PizzaCard key={key} id={pizza.id} image={pizza.img_url} name={pizza.name} desc={pizza.description} rating={pizza.rating} price={pizza.price} size={pizza.size} toppings={pizza.toppings} isVeg={pizza.isVeg} cartStatus="false" />
+                                        </>
+                                    )
+                                })
 
                                 : "Loading..."
                         }
+
                     </div>
                 </div>
-
-
             </div>
         </>
     )
